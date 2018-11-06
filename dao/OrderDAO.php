7bc6a51630e128bf5acf6653ef14560c7ec69a52
grosaict <grosa.ict@gmail.com>
2018-11-06 21:30:03 -0200

    Client object inserted in Order object

--- dao/OrderDAO.php
+++ dao/OrderDAO.php
@@ -7,15 +7,18 @@
     {
         public function list()
         {
-		    $query =   'SELECT  id_order, tb_order.id_client, name_client, order_status, order_amount
-                        FROM    tb_order, tb_client
-                        WHERE   tb_order.id_client = tb_client.id_client';
+		    $query =   'SELECT  id_order, id_client, order_status, order_amount
+                        FROM    tb_order';
     		$pdo = PDOFactory::getConexao();
 	    	$comando = $pdo->prepare($query);
     		$comando->execute();
             $orders = [];
+            // creating Client object
+            $dao = new ClientDAO;
+            
 		    while($row = $comando->fetch(PDO::FETCH_OBJ)){
-			    $orders [] = new Order($row->id_order, $row->id_client, $row->name_client, $row->order_status, $row->order_amount);
+                $client = $dao->read($row->id_client);
+			    $orders [] = new Order($row->id_order, $client, $row->order_status, $row->order_amount);
             }
             return $orders;
         }
@@ -41,22 +44,30 @@
 		    $comando->bindParam (":id_order", $id_order);
 		    $comando->execute();
             $result = $comando->fetch(PDO::FETCH_OBJ);
+
+            // creating Client object
             $dao = new ClientDAO;
             $client = $dao->read($result->id_client);
+
+            // creating and returning Order object
 		    return new Order($result->id_order, $client, $result->order_status, $result->order_amount);           
         }
         public function readByClient($id_client)
         {
- 		    $query =   'SELECT  id_order, tb_order.id_client, name_client, order_status, order_amount
-                        FROM    tb_order, tb_client
-                        WHERE   tb_order.id_client = tb_client.id_client';		
+            // creating Client object
+            $dao = new ClientDAO;
+            $client = $dao->read($id_client);
+
+ 		    $query =   'SELECT  id_order, id_client, order_status, order_amount
+                        FROM    tb_order
+                        WHERE   id_client = :id_client';		
             $pdo = PDOFactory::getConexao(); 
 		    $comando = $pdo->prepare($query);
 		    $comando->bindParam (":id_client", $id_client);
 		    $comando->execute();
             $orders = [];
 		    while($row = $comando->fetch(PDO::FETCH_OBJ)){
-			    $orders [] = new Order($row->id_order, $row->id_client, $row->name_client, $row->order_status, $row->order_amount);
+			    $orders [] = new Order($row->id_order, $client, $row->order_status, $row->order_amount);
             }
 		    return $orders;
         }
